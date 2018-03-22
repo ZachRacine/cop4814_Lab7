dcd43c6ebadba4c9cb3267a1400f1e056611c5d0
ZachRacine <ZRaci001@fiu.edu>
2018-03-22 12:42:37 -0400

    Add files via upload

---
+++ Game.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameLibrary
+{
+    public class Game
+    {
+        //Define properties for Game class
+        public string Team1 { get; set; }
+        public string Team2 { get; set; }
+        public int Team1Score { get; set; }
+        public int Team2Score { get; set; }
+
+        //Public default constructor for Game class
+        public Game() { }
+
+        //Constructor to initialize class properties from parameter values
+        public Game(string Team1, string Team2, int Team1Score, int Team2Score)
+        {
+            this.Team1 = Team1;
+            this.Team2 = Team2;
+            this.Team1Score = Team1Score;
+            this.Team2Score = Team2Score;
+        }
+
+        //ToString method to override default ToString
+        public override string ToString()
+        {
+            return String.Format(Team1 + " " + "(" + Team1Score + ")" + " - " + Team2 + " " + "(" + Team2Score + ")");
+        }
+
+    }
+}
---
+++ GameFactory.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace GameLibrary
+{
+    public class GameFactory
+    {
+        //Define objects for game list, stream writer, xml serializer, and a string containing filepath
+        List<Game> gameList; //list object for games
+        StreamWriter sw; //stream writer to enable writing to a file
+        XmlSerializer serial; //xmlserializer to enable serialization and deserialization of XML type files
+        const string FilePath = @"..\..\games.xml"; //string to hold the filepath of XML file
+        
+        //Method to create and populate a list of games
+        public void CreateGameList()
+        {
+            gameList = new List<Game>(); //instantiate new list object gameList
+            Game G = new Game("Heat", "Bulls", 98, 104);
+            gameList.Add(G);
+            G = new Game("Lakers", "Knicks", 82, 90);
+            gameList.Add(G);
+            G = new Game("Rockets", "Celtics", 102, 100);
+            gameList.Add(G);
+            G = new Game("Raptors", "Clippers", 88, 65);
+            gameList.Add(G);
+            G = new Game("Wizards", "Pistons", 99, 102);
+            gameList.Add(G);
+            G = new Game("Jazz", "Nuggets", 76, 70);
+            gameList.Add(G);
+        }
+
+        //Method to serialize gameList to an XML file
+        public void SerializeGameList()
+        {
+            serial = new XmlSerializer(gameList.GetType());
+            sw = new StreamWriter(FilePath);
+            serial.Serialize(sw, gameList);
+            sw.Close();
+        }
+
+        //Method to deserialize gameList from an XML file and returns a List<Game> object
+        public List<Game> DeserializeGameList(out string resultMessage)
+        {
+            try
+            {
+                gameList = new List<Game>();
+                StreamReader sr = new StreamReader(FilePath);
+                serial = new XmlSerializer(gameList.GetType());
+                gameList = (List<Game>)serial.Deserialize(sr);
+                sr.Close();
+                resultMessage = "Success.";
+                return gameList;
+            }
+            catch( Exception ex )
+            {
+                resultMessage = "Exception thrown: " + ex.Message;
+                return null;
+            }
+        }
+    }
+}
